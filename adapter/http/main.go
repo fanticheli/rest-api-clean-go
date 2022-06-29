e3cdbaa7e2aaf2ad2be544d2c4b7e908f5f1bb2d
fanticheli <igor.fanticheli@hotmail.com>
2022-06-28 21:04:08 -0300

    Initialize run project main.go

---
+++ adapter/http/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+
+	"github.com/fanticheli/rest-api-clean-go/adapter/postgres"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	conn := postgres.GetConnection(ctx)
+	defer conn.Close()
+
+	postgres.RunMigrations()
+}
